2528521d1782bf9ab0687200a56cc66d33c174df
21jxck <jac2007luca2013@gmail.com>
2025-04-08 13:45:42 +0200

    Completamento lettura da file CSV dei dati.

--- ProgettoSocket/CSVReader.java
+++ ProgettoSocket/CSVReader.java
@@ -1,13 +1,70 @@
+import java.io.*;
 import java.util.ArrayList;
 
 public class CSVReader {
     ArrayList<String> fileContent = new ArrayList<>();
+    ArrayList<String> costantFeatureKey = new ArrayList<>();
+    ArrayList<String> costantAmbientKey = new ArrayList<>();
+    ArrayList<String> costantLanguageKey = new ArrayList<>();
 
     public CSVReader() {
         readCSV();
     }
 
     public void readCSV() {
+        try(BufferedReader br = new BufferedReader(new FileReader("ProgettoSocket\\Regione-Veneto---Elenco-strutture-ricettive.csv"))) {
+            String fileLine;
+            int nLinea = 0;
+            while((fileLine = br.readLine()) != null) {
+                String[] dataTopics = fileLine.split(";");
+                if(nLinea == 0) {
+                    for(int i = 17; i <= 28; i++) {
+                        costantFeatureKey.add(dataTopics[i]);
+                    }
+
+                    for(int i = 29; i <= 39; i++) {
+                        costantAmbientKey.add(dataTopics[i]);
+                    }
+
+                    for(int i = 40; i <= 43; i++) {
+                        costantLanguageKey.add(dataTopics[i]);
+                    }
+                    nLinea++;
+                    continue;
+                }
+
+                Data data = new Data();
+                data.setComune(dataTopics[0]);
+                data.setProvincia(dataTopics[1]);
+                data.setTipologia(dataTopics[3]);
+                data.setCategoria(dataTopics[5]);
+                data.setNumeroStelle(dataTopics[6]);
+                data.setDenominazione(dataTopics[7]);
+                data.setIndirizzo(dataTopics[8]);
+                data.setNumeroCivico(dataTopics[9]);
+                data.setCap(dataTopics[11]);
+                data.setNumTelefono(dataTopics[12]);
+                data.setFax(dataTopics[13]);
+                data.setPostaElettronica(dataTopics[14]);
+                data.setZona(dataTopics[16]);
+
+                for(int i = 17; i <= 28; i++) {
+                    data.setFeature(costantFeatureKey.get(i), dataTopics[i]);
+                }
+
+                for(int i = 29; i <= 39; i++) {
+                    data.setAmbiente(costantAmbientKey.get(i), dataTopics[i]);
+                }
+
+                for(int i = 40; i <= 43; i++) {
+                    data.setLingua(costantLanguageKey.get(i), dataTopics[i]);
+                }
+
+                data.setCodice(dataTopics[46]);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
 
     }
 }
--- ProgettoSocket/Data.java
+++ ProgettoSocket/Data.java
@@ -4,19 +4,152 @@ public class Data {
     public String comune;
     public String provincia;
     public String tipologia;
-    public int categoria;
+    public String categoria;
     public String numeroStelle;
     public String denominazione;
     public String indirizzo;
-    public int numeroCivico;
-    public int cap;
-    public int numTelefono;
-    public int fax;
+    public String numeroCivico;
+    public String cap;
+    public String numTelefono;
+    public String fax;
     public String postaElettronica;
     public String zona;
-    public HashMap<String, Boolean> feature;
-    public HashMap<String, Boolean> ambiente;
-    public String lingua;
+    public HashMap<String, Boolean> features;
+    public HashMap<String, Boolean>  ambienti;
+    public HashMap<String, Boolean> lingue;
     public String codice;
-    public int row;
+
+    public Data() {
+    }
+
+    public void setComune(String comune) {
+        this.comune = comune;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public void setTipologia(String tipologia) {
+        this.tipologia = tipologia;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setNumeroStelle(String numeroStelle) {
+        this.numeroStelle = numeroStelle;
+    }
+
+    public void setDenominazione(String denominazione) {
+        this.denominazione = denominazione;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public void setNumeroCivico(String numeroCivico) {
+        this.numeroCivico = numeroCivico;
+    }
+
+    public void setCap(String cap) {
+        this.cap = cap;
+    }
+
+    public void setNumTelefono(String numTelefono) {
+        this.numTelefono = numTelefono;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public void setPostaElettronica(String postaElettronica) {
+        this.postaElettronica = postaElettronica;
+    }
+
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+
+    public void setFeature(String feature, String presente) {
+        this.features.put(feature, contenuto(presente));
+    }
+
+    public void setAmbiente(String ambiente, String presente) {
+        this.ambienti.put(ambiente, contenuto(presente));
+    }
+
+    public void setLingua(String lingua, String presente) {
+        this.lingue.put(lingua, contenuto(presente));
+    }
+
+    public void setCodice(String codice) {
+        this.codice = codice;
+    }
+
+    public Boolean contenuto(String presente) {
+        if(presente.equalsIgnoreCase("Si")) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getComune() {
+        return comune;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public String getTipologia() {
+        return tipologia;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getNumeroStelle() {
+        return numeroStelle;
+    }
+
+    public String getDenominazione() {
+        return denominazione;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public String getNumeroCivico() {
+        return numeroCivico;
+    }
+
+    public String getCap() {
+        return cap;
+    }
+
+    public String getNumTelefono() {
+        return numTelefono;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public String getPostaElettronica() {
+        return postaElettronica;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
 }
